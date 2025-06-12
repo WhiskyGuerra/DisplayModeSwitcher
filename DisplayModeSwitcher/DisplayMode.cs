43244a580d027f072b78c99da616cabec8d90187
WhiskyGuerra <154611700+WhiskyGuerra@users.noreply.github.com>
2025-06-12 21:48:32 +0200

    added dynamic recolutions from windows, instead of json list

---
+++ DisplayModeSwitcher/DisplayMode.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public class DisplayMode
+{
+    public string Label { get; set; }
+    public uint Width { get; set; }
+    public uint Height { get; set; }
+    public uint Frequency { get; set; }
+
+    public override string ToString()
+    {
+        return Label;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return obj is DisplayMode other &&
+               Width == other.Width &&
+               Height == other.Height &&
+               Frequency == other.Frequency;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Width, Height, Frequency);
+    }
+}
