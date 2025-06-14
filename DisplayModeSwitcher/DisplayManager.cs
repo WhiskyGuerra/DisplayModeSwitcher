4030aec8f21153412c419daa314589d4e1823fe7
WhiskyGuerra <154611700+WhiskyGuerra@users.noreply.github.com>
2025-06-14 08:30:56 +0200

    fixed duplicate

--- DisplayModeSwitcher/DisplayManager.cs
+++ DisplayModeSwitcher/DisplayManager.cs
@@ -159,23 +159,5 @@ namespace DisplayModeSwitcher
                         .ThenByDescending(m => m.Frequency)
                         .ToList();
         }
-
-        public static DisplayMode? GetCurrentDisplayMode()
-        {
-            DEVMODE dm = new DEVMODE();
-            dm.dmSize = (short)Marshal.SizeOf(typeof(DEVMODE));
-
-            if (!EnumDisplaySettings(null, ENUM_CURRENT_SETTINGS, ref dm))
-                return null;
-
-            return new DisplayMode
-            {
-                Width = (uint)dm.dmPelsWidth,
-                Height = (uint)dm.dmPelsHeight,
-                Frequency = (uint)dm.dmDisplayFrequency,
-                Label = $"{dm.dmPelsWidth}x{dm.dmPelsHeight} @ {dm.dmDisplayFrequency}Hz"
-            };
-        }
-
     }
 }
